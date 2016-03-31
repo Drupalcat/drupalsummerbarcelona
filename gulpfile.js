34851c22d05ab94ed3f5d7ecd180e1ec37474dcf
cristina <cristina.chumillas@ymbra.com>
2016-03-31 20:07:52 +0200

    Added logo, location, languages and node-sass tools.

---
+++ gulpfile.js
@@ -0,0 +1,13 @@
+'use strict';
+var gulp = require('gulp');
+
+var sass = require('gulp-sass');
+gulp.task('sass', function () {
+  gulp.src('./assets/sass/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./assets/css'));
+});
+
+gulp.task('sass:watch', function () {
+  gulp.watch('./sass/**/*.scss', ['sass']);
+});
